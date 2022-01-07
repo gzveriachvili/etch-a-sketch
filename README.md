dd23af18e1edd0c219545cc986210bb2317bae8f
gzver <gzveriachvili@gmail.com>
2022-01-07 12:02:20 +0200

    Canvas resizing via slider works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# etch-a-sketch
+# Etch-a-sketch
+
+"Etch-a-sketch" drawing app created in JavaSCript as part of The Odin Project curriculum.
--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 var slider = document.getElementById('slider');
 var output = document.getElementById('slideroutput');
+
 output.innerHTML = `${slider.value} x ${slider.value}`;
 
+const container = document.querySelector('#container');
+const cells = container.querySelectorAll('.cell');
+
+/*Create initial grid */
+for (let i = 0; i < slider.value * slider.value; i++) {
+  const newCell = document.createElement('div');
+  newCell.classList.toggle('cell');
+  container.appendChild(newCell);
+}
+
+/*Update grid**/
+const updateGrid = () => {};
 slider.oninput = function () {
-  output.innerHTML = `${this.value} x ${this.value}`;
+  let sliderValue = this.value;
+  output.innerHTML = `${sliderValue} x ${sliderValue}`;
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+
+  for (let i = 0; i < slider.value * slider.value; i++) {
+    const newCell = document.createElement('div');
+    newCell.classList.toggle('cell');
+    newCell.setAttribute('style', '');
+    container.appendChild(newCell);
+  }
+
+  container.style.setProperty(
+    'grid-template-columns',
+    'repeat(' + sliderValue + ', 1fr)'
+  );
 };
